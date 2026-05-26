c60d6ad991bd3d5d053261a97ccd892edab72a37
Szymon Kieliński <18365564+ki3lich@users.noreply.github.com>
2026-05-26 07:29:58 +0200

    refactor(test): rename test to indicate api key requirement

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -73,7 +73,7 @@ async fn fetch_yt_feed_by_channel_id_url_ok_requires_api_key() {
 }
 
 #[actix_rt::test]
-async fn fetch_yt_feed_by_playlist_url_ok() {
+async fn fetch_yt_feed_by_playlist_url_ok_requires_api_key() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
